69dcc701235d3e9ee2455de0627e7decdad6dc3a
riabovol_dmitriy <ryabovoldima@gmail.com>
2020-07-07 15:57:05 +0300

    add animation in mobile

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
    animate();
    anchor();
    upbtn();
-   formsend();
 });
 
 function animate() {
-   if ($(window).width() >= 992) {
-      new WOW().init();
-   }
+   // if ($(window).width() >= 992) {
+   //    new WOW().init();
+   // }
+   new WOW().init();
 }
 
 function anchor() {
@@ -34,27 +34,5 @@ function upbtn() {
       var id = $(this).attr('href'),
           top = $(id).offset().top;
       $('html, body, .wrapper').animate({scrollTop: top}, 1000);
-   });
-}
-
-function formsend() {
-   $(document).ready(function() {
-      //E-mail Ajax Send
-      $("form").submit(function() { //Change
-         var th = $(this);
-         $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-         }).done(function() {
-            $('.contact__send').addClass('thanks');
-            setTimeout(function() {
-               // Done Functions
-               th.trigger("reset");
-            }, 1000);
-         });
-         return false;
-      });
-   
    });
 }
